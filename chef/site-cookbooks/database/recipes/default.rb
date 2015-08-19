e6550d4bb31a34e037dcf7f7579e1198a13a7d90
david jimenez <david.jimenez19@gmail.com>
2015-08-20 01:16:13 +0200

    Mysql installed with a custom cookbook
    The grant action is not working, unknown error

--- chef/site-cookbooks/database/recipes/default.rb
+++ chef/site-cookbooks/database/recipes/default.rb
@@ -8,28 +8,28 @@
 mysql_connection_info = {
     :host     => "localhost",
     :username => 'root',
-    :password => node['mysql']['server_root_password']
+    :password => 'change me'
 }
 
-# # 'rails'@'localhost'
-# mysql_database_user 'rails' do
-#   connection mysql_connection_info
-#   password ''
-#   host 'localhost'
-#   privileges ["ALL PRIVILEGES"]
-#   grant_option true
-#   action :grant
-# end
-#
-# # 'rails'@'%'
-# mysql_database_user 'rails' do
-#   connection mysql_connection_info
-#   password ''
-#   host '%'
-#   privileges ["ALL PRIVILEGES"]
-#   grant_option true
-#   action :grant
-# end
+# 'rails'@'localhost'
+mysql_database_user 'rails' do
+  connection mysql_connection_info
+  password ''
+  host 'localhost'
+  privileges ["ALL PRIVILEGES"]
+  grant_option true
+  action :grant
+end
+
+# 'rails'@'%'
+mysql_database_user 'rails' do
+  connection mysql_connection_info
+  password ''
+  host '%'
+  privileges ["ALL PRIVILEGES"]
+  grant_option true
+  action :grant
+end
 
 # PostgreSQL
 template "#{node[:postgresql][:dir]}/pg_hba.conf" do
--- chef/site-cookbooks/mysql_config/recipes/default.rb
+++ chef/site-cookbooks/mysql_config/recipes/default.rb
@@ -1,6 +1,19 @@
+
+version = '5.5'
+# node['mysql']['server_root_password'] = 'change me'
+
+node.default['mysql'] = {
+    'server_root_password' => 'change me'
+}
+mysql_client 'default' do
+  version version
+  action [:create]
+end
+
 mysql_service 'default' do
-  version '5.5'
+  version version
   port '3306'
+  initial_root_password node['mysql']['server_root_password']
   action [:create, :start]
 end
 
